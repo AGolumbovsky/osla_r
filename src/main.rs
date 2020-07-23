cbfe6ead87492e53af65de74a3523b589a9164dd
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-07-23 17:36:26 -0400

    rocket tuts stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Behold the Placeholder!!!"
+}
+
+#[get("/elo")]
+fn elo() -> &'static str {
+    "elo you"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/elo", routes![index, elo]).launch();
 }
