27f69c4b0fb0357332fa9fccad3960a48b3c38e6
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-08-16 23:40:33 -0400

    i don't even know wtf i'm doing... migrations

--- migrations/2020-08-15-231426_osla/up.sql
+++ migrations/2020-08-15-231426_osla/up.sql
@@ -1,8 +1,10 @@
 -- Your SQL goes here
 CREATE TABLE test_osla_diesel (
+
   id SERIAL PRIMARY KEY,
   word VARCHAR NOT NULL,
   part_of_speech TEXT NOT NULL,
   meaning TEXT NOT NULL,
   approved BOOLEAN NOT NULL DEFAULT 'f'
+  
 )
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@
 
 #[macro_use] extern crate rocket_contrib;
 
-use rocket_contrib::databases::diesel;
+// use rocket_contrib::databases::diesel;
+
+#[macro_use] extern crate diesel;
 
 extern crate dotenv;
 
